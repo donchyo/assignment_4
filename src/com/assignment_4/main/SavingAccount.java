b347bb4f12b3e1fa87d702b0cf1b5b17fe68a5ea
donchyo <daniel.eke.eriksson@gmail.com>
2016-10-04 13:32:33 +0200

    This is the first commit on the mandatory 04 project

---
+++ src/com/assignment_4/main/SavingAccount.java
@@ -0,0 +1,11 @@
+package com.assignment_4.main;
+import java.util.UUID;
+import com.assignment_4.subclasses.BankAccount;
+
+public class SavingAccount extends BankAccount{
+	public SavingAccount() {	
+		this.setAccountNumber(UUID.randomUUID().toString().substring(0, 6));
+		this.setBalance(0.0);
+		this.setAccountType("Saving account");
+	}
+}
