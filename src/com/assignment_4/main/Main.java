6ce8474473c57cdf693866acdd048c3d1619a4f6
donchyo <daniel.eke.eriksson@gmail.com>
2016-10-06 15:54:10 +0200

    This is hopefully the final commit! :D

--- src/com/assignment_4/main/Main.java
+++ src/com/assignment_4/main/Main.java
@@ -4,27 +4,14 @@ import com.assignment_4.subclasses.BankCustomer;
 import com.assignment_4.subclasses.PersonalAccount;
 import com.assignment_4.subclasses.SavingAccount;
 import com.assignment_4.superclasses.BankAccount;
-
+/**
+ * 
+ * @author Viktor Löfgren and Daniel Eriksson
+ *
+ */
 public class Main {
 	public static void main(String[] args){
-		/*
-		BankCustomer Daniel = new BankCustomer("Daniel", 21);
-		System.out.println(Daniel.toString());
 		
-		BankAccount bankAccount = new BankAccount();
-		System.out.println(bankAccount.toString());
-		bankAccount.depositMoney(200.0);
-		
-		PersonalAccount pa = new PersonalAccount();
-		pa.depositMoney(70);
-		System.out.println(pa.toString());
-		pa.depositMoney(-1);
-		
-		SavingAccount sa = new SavingAccount();
-		sa.withdrawMoney(-20);
-		sa.withdrawMoney(20);
-		System.out.println(sa.toString());
-		*/
 		
 		String account = "";
 		
--- src/com/assignment_4/subclasses/BankCustomer.java
+++ src/com/assignment_4/subclasses/BankCustomer.java
@@ -4,24 +4,50 @@ import java.util.ArrayList;
 
 import com.assignment_4.superclasses.BankAccount;
 import com.assignment_4.superclasses.Human;
-
+/**
+ * 
+ * @author Daniel Eriksson, Viktor Löfgren
+ *
+ */
 public class BankCustomer extends Human{
 	
 	private ArrayList<BankAccount> customerAccounts = new 
 			ArrayList<BankAccount>();
-		
+	/**
+	 * Bank customer, takes a string name and int age to create a BankCustomer
+	 * @param name
+	 * @param age
+	 */
 	public BankCustomer(String name, int age) {
 		super(name, age);
 	}
+	/**
+	 * getter customer accounts
+	 * @return
+	 */
 	public ArrayList<BankAccount> getCustomerAccounts(){
 		return customerAccounts;
 	}
+	/**
+	 * setter customeraccounts
+	 * @param customerAccounts
+	 */
 	public void setCustomerAccounts(ArrayList<BankAccount> customerAccounts) {
 		this.customerAccounts = customerAccounts;
 	}
+	/**
+	 * 
+	 * adds a object bank account to the arraylist of bank accounts
+	 * @param bankAccount
+	 */
 	public void addAccount(BankAccount bankAccount){
 		customerAccounts.add(bankAccount);
 	}
+	/**
+	 * checks if the accountname entered exists in the arraylist of accounts, then deposits money into the account.
+	 * @param accountName
+	 * @param amount
+	 */
 	public void depositToAccount(String accountName, double amount){
 		for (int i = 0; i < customerAccounts.size(); i++){
 			if(customerAccounts.get(i).getAccountNumber().equals(accountName)){
@@ -29,6 +55,11 @@ public class BankCustomer extends Human{
 			}
 		}
 	}
+	/**
+	 * uses withdrawMoney to withdraw money from an accountname if it exists in the arraylist with accounts
+	 * @param accountName
+	 * @param amount
+	 */
 	public void withdrawMoneyFromAccount(String accountName, double amount){
 		for (int i = 0; i < customerAccounts.size(); i++){
 			if(customerAccounts.get(i).getAccountNumber().equals(accountName)){
@@ -36,7 +67,9 @@ public class BankCustomer extends Human{
 			}
 		}
 	}
-	
+	/**
+	 * toSting, will return a string with a customers, name, age aswell as their bank accounts with all the info within.
+	 */
     public String toString() {
         String result = "Customer: "+getName()+", Age: "+getAge()+"\n";
         for (int i = 0; i < customerAccounts.size(); i++) {
